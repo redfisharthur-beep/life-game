adcd3c51f603cc37bda6eea46cddf3759c98e47a
redfisharthur-beep <redfisharthur@gmail.com>
2026-08-30 14:40:37 +0800

    Use self-healing GameRoom recovery wrapper

--- src/worker.js
+++ src/worker.js
@@ -1,4 +1,4 @@
-export { GameRoom } from './game-room-rules-v2.js';
+export { GameRoom } from './game-room-recovery.js';
 export { Matchmaker } from './matchmaker.js';
 
 function json(data, status = 200) {
